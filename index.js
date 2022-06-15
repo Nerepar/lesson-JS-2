152004a11b1cdc2a74873b17eb3673d1789e12ed
Nerepar <fedorin.denis.1@yandex.ru>
2022-06-15 19:27:38 +0500

    lesson-js-2

---
+++ index.js
@@ -0,0 +1,18 @@
+// let date = new Date();
+//
+// console.log(date);
+// console.log(date.getFullYear());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+//
+// let date3 = new Date();
+// date3.setDate(0);
+// console.log(date3);
+//
+// let date4 = new Date();
+// date4.setMonth(3);
+// console.log(date4);
+//
+// let arr = [1, 2, 3, 4];
+//
+// console.log(arr);
